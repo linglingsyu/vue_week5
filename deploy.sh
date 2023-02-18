65e907675e9f799645303ad6f35ba84544d7ea81
elsa <elsa.syu@dexatek.com>
2023-02-18 15:37:40 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/linglingsyu/vue_week4.git main:gh-pages
+git push -f git@github.com:linglingsyu/vue_week5.git main:gh-pages
 
 cd -
